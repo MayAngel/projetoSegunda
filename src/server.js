d8bacbac770530527ad055388a378fbd186b13e3
Kleyton Klein <17971295+Tkporto@users.noreply.github.com>
2021-05-28 18:41:38 -0300

    Revert "Criação de dados e criação da resultado.ejs"
    This reverts commit 319f78b1bd964591ba2bce3e6d2b425f43f88496.

--- src/server.js
+++ src/server.js
@@ -6,77 +6,57 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../src')));
 
-//Apontamento da engine do EJS
-app.set("view engine", "ejs");
-app.set("views", (path.join(__dirname, './views')));
+//ROTAS
+app.get("/" , (request, response)=>{
+    response.sendFile(path.join(__dirname, './assets/html/index.html'));
+}); 
 
-//INICIO ROTAS
-
-//INICIO INDEX
-app.get("/", (request, response) => {
-    response.render("index");
-});
-
-
-//FIM INDEX
-//INICIO BUSCAR
-app.get("/buscar", (request, response) => {
+app.get("/buscar" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/buscar.html'));
-});
-//FIM BUSCAR
-//INICIO CADASTRO
-app.get("/cadastro", (request, response) => {
+}); 
+//Inicio Cadastro
+app.get("/cadastro" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/cadastro.html'));
 });
-app.post("/cadastro", (request, response) => {
+app.post("/cadastro" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/cadastro.html'));
 });
-//FIM CADASTRO
-//INICIO CADASTRO USUARIO
-app.get("/cadastroUsuario", (request, response) => {
+//Fim Cadastro
+//Inicio Cadastro de Usuario
+app.get("/cadastroUsuario" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/cadastroUsuario.html'));
-});
+}); 
 
 //app.post("/cadastroUsuario" , (request, response)=>{
 //    response.sendFile(path.join(__dirname, './assets/html/cadastroUsuario.html'));
 //}); 
+//Fim Cadastro de Usuario
 
-//FIM CADASTRO USUARIO
-//INICIO DETALHES DO SERVICO
-app.get("/detalhesDoServico", (request, response) => {
+app.get("/detalhesDoServico" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/detalhesDoServico.html'));
-});
-//FIM DETALHES DO SERVICO
-//INICIO LOGIN
-app.get("/login", (request, response) => {
+}); 
+
+app.get("/login" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/login.html'));
 });
-app.post("/login", (request, response) => {
+app.post("/login" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/login.html'));
 });
-//FIM LOGIN
-//INICIO MISSAO
-app.get("/missao", (request, response) => {
+
+
+app.get("/missao" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/missao.html'));
-});
-//FIM MISSAO
-//INICIO QUEM SOMOS
-app.get("/quemsomos", (request, response) => {
+}); 
+
+app.get("/quemsomos" , (request, response)=>{
     response.sendFile(path.join(__dirname, './assets/html/quemsomos.html'));
-});
-//FIM QUEM SOMOS
-//INICIO RESULTADO
-const servicos = require("./assets/dados/servicos.json");
-const titulo = {titulo: "SERVIÇOS ENCONTRADOS"};
+}); 
 
-app.get("/resultado", (request, response) => {
-    response.render("./layout/template", { conteudo: "resultado", servicos: servicos , titulo: titulo });
-});
+app.get("/resultado" , (request, response)=>{
+    response.sendFile(path.join(__dirname, './assets/html/resultado.html'));
+}); 
 
-//FIM RESULTADOS
-//FIM ROTAS
 
-//CHAMADA DO SERVIDOR
-app.listen(3031, function () {
+app.listen(3031, function(){
     console.log("Servidor esta rodando na porta 3031");
 });
